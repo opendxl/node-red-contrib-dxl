3070b953836c159e0f5cde44513906975bf91700
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-05-22 11:06:55 -0700

    Add removeProperties NodeUtils function
    This commit adds a removeProperties function to NodeUtils. The
    removeProperties function is similar to extractProperties except that
    the properties removed from the object are not returned to the caller.

--- test/node-utils-test.js
+++ test/node-utils-test.js
@@ -84,4 +84,11 @@ describe('NodeUtils', function () {
       obj.should.eql({prop2: 'val2'})
     })
   })
+  context('.removeProperties', function () {
+    it('should remove properties from an object', function () {
+      var obj = {prop1: 'val1', prop2: 'val2', prop3: 'val3'}
+      NodeUtils.removeProperties(obj, ['prop1', 'prop3'])
+      obj.should.eql({prop2: 'val2'})
+    })
+  })
 })
